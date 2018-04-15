93988c02fbbcb237c1af5ca250e3cb574923a174
Paul Borawski <paulborawski14@gmail.com>
2018-04-14 17:02:19 -0700

    cluster.fork() creating 2 instances of node

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-const express = require('express');
-const app = express();
-const port = 4999;
+const cluster = require('cluster');
 
-function doWork(duration) {
-  const start = Date.now();
-  while(Date.now() - start < duration) {}
-  console.log('done')
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+  // Cause index.js to be executed *again*
+  // but in child mode.
+  cluster.fork();
+  cluster.fork();
+} else {
+  // I'm a child and I'm going to act like a server
+  // and do nothing else
+  const express = require('express');
+  const app = express();
+  const port = 4999;
+
+  function doWork(duration) {
+    const start = Date.now();
+    while(Date.now() - start < duration) {}
+    console.log('done')
+  }
+
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('Hi there');
+  })
+
+  app.get('/fast', (req, res) => {
+    res.send('this was fast');
+  })
+
+  app.listen(port)
+
+  console.log('app is running on port:', port);
 }
-
-app.get('/', (req, res) => {
-  doWork(5000);
-  res.send('Hi there');
-})
-
-app.listen(port)
-
-console.log('app is running on port:', port);
