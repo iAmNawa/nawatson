1e839266ce8ce370655c2e155e5c273346680bd6
Paul Borawski <paulborawski14@gmail.com>
2018-04-15 08:34:06 -0700

    checking cluster.isMaster and forcing ALL children to have a threadpool size of 1

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require('cluster');
 
 // Is the file being executed in master mode?
 if (cluster.isMaster) {
+  console.log(cluster.isMaster);
   // Cause index.js to be executed *again*
   // but in child mode.
   cluster.fork();
-  cluster.fork();
 } else {
+  console.log(cluster.isMaster);
   // I'm a child and I'm going to act like a server
   // and do nothing else
   const express = require('express');
+  const crypto = require('crypto');
   const app = express();
   const port = 4999;
 
-  function doWork(duration) {
-    const start = Date.now();
-    while(Date.now() - start < duration) {}
-    console.log('done')
-  }
-
   app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hi there');
-  })
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+      res.send('Hi there');
+    });
+  });
 
   app.get('/fast', (req, res) => {
     res.send('this was fast');
